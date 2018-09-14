beb948e23c738423c893b677dde656faa73399cc
askbk <askbk@protonmail.com>
2018-09-14 12:24:11 +0200

    added authentication check on front page

--- js/home.js
+++ js/home.js
@@ -1,1 +1,28 @@
-function home() {}
+function home() {
+    HomeModel.getHomePage();
+}
+
+let HomeModel = {
+    getHomePage : function () {
+        return new Promise(function(resolve, reject) {
+            $.ajax({
+                url: 'php/getVideos.php',
+                beforeSend: function(request){
+                    request.setRequestHeader('Authorization', 'Bearer ' + localStorage.jwt);
+                },
+                type: 'POST',
+                success: function(data) {
+                    resolve(true);
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                    if (xhr.status == 401) {
+                        console.log("not logged in");
+                        location.hash = "/innlogging";
+                    } else {
+                        console.log("error: " + xhr.status);
+                    }
+                }
+            });
+        });
+    }
+};
