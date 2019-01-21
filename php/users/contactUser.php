c508c9bbdb483665e1786f3947ba181aeb6fc350
askbk <askbk@protonmail.com>
2019-01-21 22:13:00 +0100

    La til mulighet for kontaktforespørsler i server-APIet

---
+++ php/users/contactUser.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../class/Auth.php';
+require_once '../class/User.php';
+
+if (Auth::isLoggedIn()) {
+    $user_id = Auth::getUserId();
+    $params = json_decode(stripslashes(file_get_contents("php://input")));
+    $receiver_id = $params->receiver;
+
+    if (!User::sendContactRequest($user_id, $receiver_id);) {
+        echo "Users already have contact";
+        exit();
+    }
+
+    echo "Successful contact request";
+}
+
+?>
