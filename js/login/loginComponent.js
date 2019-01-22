cb8eb8121af06ddd8ad14af87ceded3dde55d4b2
askbk <askbk@protonmail.com>
2019-01-22 23:18:17 +0100

    Byttet ut jquery sin serialize-funksjonalitet med egen.

--- js/login/loginComponent.js
+++ js/login/loginComponent.js
@@ -1,4 +1,5 @@
 import { Component } from '../component.js';
+import { Forms } from '../forms.js';
 
 export class LoginComponent extends Component{
     constructor(DEBUG_MODE, authService, router) {
@@ -8,13 +9,13 @@ export class LoginComponent extends Component{
     }
 
     init() {
-        $(document).on("click", "input[name='loginBtn']", e => {
+        document.querySelector("button[name='loginBtn']").addEventListener("click", e => {
             e.preventDefault();
-            this.authService.login($("#loginForm").serialize())
+            this.authService.login(Forms.serialize("#loginForm"))
             .then(() => {
                 this.router.navigate("hjem");
             });
-        });
+        })
     }
 
     destroy() {
