13cbf97fef095ebca46765eb3052a00ffaecfe8d
ask <askbk@protonmail.com>
2019-01-26 14:32:18 +0100

    Fikset sletting av filer fra databasen

--- php/users/deleteFile.php
+++ php/users/deleteFile.php
@@ -3,8 +3,9 @@ require_once '../class/Auth.php';
 require_once '../class/User.php';
 
 if (Auth::isLoggedIn()) {
+    $params = json_decode(stripslashes(file_get_contents("php://input")));
+    $fileId = $params->fileId;
     $user_id = Auth::getUserId();
-    $fileId = $_POST["userFile_id"];
 
     User::deleteFile($user_id, $fileId);
 }
