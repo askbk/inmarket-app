3672425da74dfbf6941e5b41b1e6ae037c65a786
askbk <askbk@protonmail.com>
2019-01-27 22:31:33 +0100

    La til grunnleggende funksjonalitet i nettverksmodul

--- js/network/network.module.js
+++ js/network/network.module.js
@@ -1,10 +1,21 @@
 import { Module } from '../module.js';
 import { Route } from '../router.js';
+import { RequestsComponent } from './requests.component.js';
 
 export class NetworkModule extends Module {
     constructor(DEBUG_MODE, appRouter, networkService, pattern) {
+        const networkPages = [
+            new RequestsComponent(
+                DEBUG_MODE,
+                networkService,
+                appRouter,
+                pattern
+            )
+        ];
         super(DEBUG_MODE, appRouter,
-            []
+            [
+                new Route(/nettverk\/?/, networkPages[0])
+            ]
         );
         this.networkService = networkService;
     }
