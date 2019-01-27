80aa042c8ccad9f2f8428455080bec54963e930b
ask <askbk@protonmail.com>
2019-01-27 21:50:11 +0100

    Lagde filer for nettverksmodul

---
+++ js/network/requests.component.js
@@ -0,0 +1,12 @@
+import { Component } from '../component.js';
+import { Template } from './template.js';
+
+export class RequestsComponent extends Component {
+    constructor(DEBUG_MODE, appRouter, profileService) {
+        super(DEBUG_mode, {}, {}, undefined, undefined)
+    }
+
+    init() {
+
+    }
+}
